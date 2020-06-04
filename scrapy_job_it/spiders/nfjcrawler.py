b3bd9a3465957ded56a7a5547f1407ec4f9cc5ee
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-06-04 22:38:34 +0200

    nothing2

--- scrapy_job_it/spiders/nfjcrawler.py
+++ scrapy_job_it/spiders/nfjcrawler.py
@@ -3,7 +3,6 @@ import os
 import scrapy
 from scrapy_selenium import SeleniumRequest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from ..items import ScrapyJobItItem
 from scrapy.selector import Selector
