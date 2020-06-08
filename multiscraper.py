5f2752eda6e8e88a62299d09678b62ed8d15b86f
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-06-08 22:43:53 +0200

    multiscraper changed

--- multiscraper.py
+++ multiscraper.py
@@ -1,12 +1,23 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from twisted.internet import reactor, defer
+from scrapy.crawler import CrawlerRunner
 # Import every spider
 from scrapy_job_it.spiders.bdcrawler import BDcrawlerSpider
 from scrapy_job_it.spiders.nfjcrawler import NfjcrawlerSpider
 
 #settings = get_project_settings()
-process = CrawlerProcess()
+# process = CrawlerProcess()
 # Insert a crawl process for every spider here
-process.crawl(BDcrawlerSpider)
-process.crawl(NfjcrawlerSpider)
-process.start()
+# process.crawl(BDcrawlerSpider)
+# process.crawl(NfjcrawlerSpider)
+# process.start()
+runner = CrawlerRunner()
+@defer.inlineCallbacks
+def crawl():
+    yield runner.crawl(BDcrawlerSpider)
+    yield runner.crawl(NfjcrawlerSpider)
+    reactor.stop()
+
+crawl()
+reactor.run() # the script will block here until the last crawl call is finished
