f5e31fcb27cfba0718228c2e460a3f37e69e6dea
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-06-08 21:30:10 +0200

    crawl with settings

--- multiscraper.py
+++ multiscraper.py
@@ -1,10 +1,11 @@
 from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 # Import every spider
 from scrapy_job_it.spiders.bdcrawler import BDcrawlerSpider
 from scrapy_job_it.spiders.nfjcrawler import NfjcrawlerSpider
 
-
-process = CrawlerProcess()
+settings = get_project_settings()
+process = CrawlerProcess(settings)
 # Insert a crawl process for every spider here
 process.crawl(BDcrawlerSpider)
 process.crawl(NfjcrawlerSpider)
