0697d4051cd9d551eb3a66af7067e3e756ea8e15
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-06-08 22:27:26 +0200

    no settints in crawl

--- multiscraper.py
+++ multiscraper.py
@@ -4,8 +4,8 @@ from scrapy.utils.project import get_project_settings
 from scrapy_job_it.spiders.bdcrawler import BDcrawlerSpider
 from scrapy_job_it.spiders.nfjcrawler import NfjcrawlerSpider
 
-settings = get_project_settings()
-process = CrawlerProcess(settings)
+#settings = get_project_settings()
+process = CrawlerProcess()
 # Insert a crawl process for every spider here
 process.crawl(BDcrawlerSpider)
 process.crawl(NfjcrawlerSpider)
