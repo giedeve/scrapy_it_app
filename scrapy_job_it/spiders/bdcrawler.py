a583926e818b9ce0a18ad109d76bae94ebb44fe1
KasiaBotkowska <katarzyna.botkowska@gmail.com>
2020-09-24 20:00:56 +0200

    bdcrawler update

--- scrapy_job_it/spiders/bdcrawler.py
+++ scrapy_job_it/spiders/bdcrawler.py
@@ -60,14 +60,14 @@ class BDcrawlerSpider(scrapy.Spider):
                 offert = ScrapyJobItItem()
                 offert['title'] = item.xpath(f"normalize-space({self.JOB_DETAILS_DIV}/div[@class='title']/h2/text())").get()
                 offert['salary_range'] = ''.join(
-                        item.xpath(f"{self.JOB_DETAILS_DIV}/{self.META_DIV}/div[@class='salary']/text()").extract())
+                    item.xpath(f"{self.JOB_DETAILS_DIV}/{self.META_DIV}/div[@class='salary']/text()").extract())
                 offert['company'] = item.xpath(
-                        f"normalize-space({self.JOB_DETAILS_DIV}/{self.META_DIV}/div[@class='company']/text())").get()
+                    f"normalize-space({self.JOB_DETAILS_DIV}/{self.META_DIV}/div[@class='company']/text())").get()
                 offert['city'] = item.xpath(
-                        f"normalize-space({self.JOB_DETAILS_DIV}/{self.META_DIV}/div[@class='location']//following-sibling::text())").get()
+                    f"normalize-space({self.JOB_DETAILS_DIV}/{self.META_DIV}/div[@class='location']//following-sibling::text())").get()
 
                 offert['keywords'] = item.xpath(
-                        "./div[@class='technologies']/ul[@class='tags']/child::li/div/text()").extract()
+                    "./div[@class='technologies']/ul[@class='tags']/child::li/div/text()").extract()
                 offert['job_url'] = item.xpath('.//@href').get()
                 offert['scrapped'] = True
                 offert['still_active'] = True
@@ -87,8 +87,12 @@ class BDcrawlerSpider(scrapy.Spider):
                 # }
 
             # Go to the next page
-            next_button = driver.find_element_by_xpath(
-                ".//ul[@class='pagination']/li[@class='active']/following-sibling::li[1]/a")
+            try:
+                next_button = driver.find_element_by_xpath(
+                    ".//ul[@class='pagination']/li[@class='active']/following-sibling::li[1]/a")
+            except Exception:
+                print('It was last page...')
+                break
             driver.execute_script("arguments[0].click();", next_button)
 
             time.sleep(self.PAUSE_TIME)
